e7a47d0d080c6c25d09c00d612c3fcbd16dda497
mateopedehonta <mateopedehonta@gmail.com>
2023-09-19 15:26:38 -0300

    cambio

--- src/components/ui/modals/MyModal.tsx
+++ src/components/ui/modals/MyModal.tsx
@@ -9,7 +9,7 @@ import {
   Button,
 } from "@chakra-ui/react"
 import { ReactElement, Children, cloneElement } from "react"
-import { Sizes } from "schemas/UiSchemas"
+import { Sizes } from "~/schemas/UiSchemas"
 
 interface Props {
   title: string
